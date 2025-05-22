aad4da6b1d64fa1b8a2cde7c925fcccdb5ff79bd
lucasbomfim15 <bmfmlucas@gmail.com>
2025-05-22 10:37:08 -0300

    feat: created exception handler middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
+import exceptionHandler from "./common/middlewares/ExceptionHandler";
+import userRoutes from "./modules/user/routes/userRoutes";
 
 dotenv.config();
 
@@ -8,6 +10,15 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Rota de teste funcionando!" });
+});
+
+app.use("/users", userRoutes);
+
+app.use(exceptionHandler);
 
 const PORT = process.env.PORT || 3000;
 
